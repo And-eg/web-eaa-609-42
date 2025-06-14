8909ee2192317f80ca6dab34a05dfb93f5898e5b
And-eg <Lex_foreve@rambler.ru>
2025-06-14 11:21:10 +0500

    format document

--- main.js
+++ main.js
@@ -1,15 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const buttonMenu = document.querySelector('.mobile-navigation__header-button');
-    const mobileNavigation = document.querySelector('.mobile-navigation');
-    
-    buttonMenu.addEventListener('click', function() {
-        mobileNavigation.classList.toggle('opened');
-    });
-    
-    const menuLinks = document.querySelectorAll('.mobile-navigation .navigation__list a');
-    menuLinks.forEach(link => {
-        link.addEventListener('click', function() {
-            mobileNavigation.classList.remove('opened');
-        });
-    });
-});
+const buttonMenu = document.querySelector('.mobile-navigation__header-button')
+const mobileNavigation = document.querySelector('.mobile-navigation')
+
+buttonMenu.addEventListener('click', buttonMenuClicked)
+
+function buttonMenuClicked() {
+    mobileNavigation.classList.toggle('opened')
+}
